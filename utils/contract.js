3ba6214673377092b734609f587b25f3c6b98b29
Sidharth P <sidharthpunathil714@gmail.com>
2023-05-09 10:47:01 +0530

    initial commit

---
+++ utils/contract.js
@@ -0,0 +1,33 @@
+import { ethers } from 'ethers'
+import CustomDexABI from '../utils/CustomDex.json'
+import CustomTokenABI from '../utils/CustomToken.json'
+
+export const tokenContract = async address => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const { ethereum } = window
+
+  if (ethereum) {
+    const signer = provider.getSigner()
+
+    const contractReader = new ethers.Contract(address, CustomTokenABI, signer)
+
+    return contractReader
+  }
+}
+
+export const contract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const { ethereum } = window
+
+  if (ethereum) {
+    const signer = provider.getSigner()
+
+    const contractReader = new ethers.Contract(
+      '0xbE4cD94f4BFa60738D30c81DC5096C7044E937dc',
+      CustomDexABI.abi,
+      signer,
+    )
+
+    return contractReader
+  }
+}
